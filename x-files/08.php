69e450ec89aaab1545fe069e5303a3f756bba491
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-10-30 18:01:06 +0100

    gg

--- x-files/08.php
+++ x-files/08.php
@@ -1,10 +1,20 @@
 <?php
 $dir = 'recources/files';
 $files = scandir($dir);
+$selectedFile = $_GET['file'] ?? null; // Get the selected file from the URL parameter
+
+// Check if the selected file exists
+if ($selectedFile !== null && file_exists($dir . '/' . $selectedFile)) {
+    $fileToDisplay = $selectedFile;
+} else {
+    // Display a custom message if the file doesn't exist
+    $fileToDisplay = 'no-valid-name.txt';
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,18 +24,22 @@ $files = scandir($dir);
 
 <body>
 <right>
-    <ul id="nav"><?php
+    <ul class="nav" id="nav"><?php
     foreach($files as $key => $file){
         if ( str_contains($file,'.txt') !== false ){?> 
-    <li><a href="recources/files/<?=$file?>" target="if1"><?=$file?></a></li><br><?php    
+    <li class="list-items"><a href="recources/files/<?=$file?>" target="if1" class="<?= $isActive ? 'active' : '' ?>"><?=$file?></a></li><br><?php    
         }
     }?>
     </ul>
 </right>
 <left>
-    <iFrame name="if1"></iframe">
+    <div id="placeholder-div">
+        Please choose a file from the list on the left.
+    </div>
+    <iframe name="if1" id="file-iframe"></iframe>
 </left>
 </body>
+</html>
 <?php
 /*
  Continue working on the previous exercise. Exclude any file in the list which doesn't have the txt file extension. 
