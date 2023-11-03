2dfe1be67168f5a7b5c0cdbdb5c0e09f770006d0
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-03 12:53:57 +0100

    v

---
+++ sessionsandcookies/resources/04.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+$user = [
+    'name' => 'Bertram Gilfoyle',
+    'email' => 'gilfoyle@piedpiper.io',
+    'password' => '$2y$10$Qx.ZsPJooxIqFDewbA9wS.fY6Nkp5qkmJynqkbwxmEyX5Q9Er5EBW',
+];
+
+$password = 'username-is-password';
+
+// TODO: Implement the login logic here.
