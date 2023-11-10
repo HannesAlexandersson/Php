8d3b48e9d9ebc5c8b8a4bcaba1a2a8e2bfdc6cf1
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-10 11:25:07 +0100

    fixes

--- PDO-ADV/05.php
+++ PDO-ADV/05.php
@@ -24,7 +24,11 @@ if (isset($_POST['search'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>05</title>    
+    <title>05</title>  
+    <link rel="Stylesheet" href="styling.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700&display=swap" rel="stylesheet">  
 </head>
 <body>
     <form action="05.php" method="post">
